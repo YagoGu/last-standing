e299539ac0eccd2e53095dde10436b1f797e1cf0
YagoGu <yago.gutierrez15@gmail.com>
2023-07-24 13:00:05 +0200

    Monster generator, time out & time life

--- script.js
+++ script.js
@@ -7,16 +7,24 @@ function monsterSpawn () {
 }
 
 //If there is no monster on the entire grid generate 1 
-if (monsterCounter === 0) {
-
-    const spawn = monsterSpawn()
-    const monster = document.createElement("img")
-    monster.src="/src/monster.gif"
-    monster.style.width = "60%"
-    console.log(spawn)
-    document.getElementsByClassName("grid-monster")[spawn].appendChild(monster)
-    //document.getElementsByClassName("grid-monster")[spawn].innerHTML = "MONSTER"
-
-    monsterCounter = 1;
-
-}
+setInterval(() => {
+    //monster time generator
+    if (monsterCounter === 0) {
+        const spawn = monsterSpawn()
+        const monster = document.createElement("img")
+        //set atributtes for monster
+        monster.id="monster"
+        monster.src="/src/monster.gif"
+        monster.style.width = "60%"
+        console.log(spawn) //testing
+        document.getElementsByClassName("grid-monster")[spawn].appendChild(monster)
+        //document.getElementsByClassName("grid-monster")[spawn].innerHTML = "MONSTER"
+    
+        monsterCounter = 1;
+    }
+    //monster time live
+    setTimeout(() => {
+        document.getElementById("monster").remove();
+        monsterCounter = 0;
+    }, 5000)
+}, 3000);
