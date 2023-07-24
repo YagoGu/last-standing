014e83dc36ddfdabf728b959597ea658d79c9cb9
YagoGu <yago.gutierrez15@gmail.com>
2023-07-24 12:38:01 +0200

    Basic monster spawning

--- script.js
+++ script.js
@@ -10,8 +10,12 @@ function monsterSpawn () {
 if (monsterCounter === 0) {
 
     const spawn = monsterSpawn()
+    const monster = document.createElement("img")
+    monster.src="/src/monster.gif"
+    monster.style.width = "60%"
     console.log(spawn)
-    document.getElementsByClassName("grid-monster")[spawn].innerHTML = "MONSTER"
+    document.getElementsByClassName("grid-monster")[spawn].appendChild(monster)
+    //document.getElementsByClassName("grid-monster")[spawn].innerHTML = "MONSTER"
 
     monsterCounter = 1;
 
