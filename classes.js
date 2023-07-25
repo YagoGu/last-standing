402ef0a6d8fbd3c4dcc1594b0de59c46386d7ae4
YagoGu <yago.gutierrez15@gmail.com>
2023-07-25 11:14:22 +0200

    Shoot animation fixed

--- classes.js
+++ classes.js
@@ -40,12 +40,10 @@ class Player {
     //shoot
     shoot() {
         //gif thingie
-        document.getElementById("shotgun").style.marginLeft = "-2vw"
-        document.getElementById("shotgun").style.maxWidth = "140%"
-        document.getElementById("shotgun").style.maxHeight = "105%"
+        document.getElementById("shotgun").style.height = "102%"
         document.getElementById("shotgun").src = "/src/shotgun.gif"
         setTimeout (() => {
-            document.getElementById("shotgun").style.maxWidth = "130%"
+            document.getElementById("shotgun").style.height = "102%"
             document.getElementById("shotgun").src = "/src/shotgun.png"
         }, 500)
         
