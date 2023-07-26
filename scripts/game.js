e3b708f9be35aae92088ef3d0c857a34dca51cd1
YagoGu <yago.gutierrez15@gmail.com>
2023-07-26 09:01:22 +0200

    Error message on re-start screen

--- scripts/game.js
+++ scripts/game.js
@@ -17,7 +17,7 @@ class Game {
                 <div class="score" id="gameOver">GAME OVER</div>
                 <div class="score" id="highScore">High score: ${highScore} points</div>
                 <div class="score" id="aliensKilled">Aliens Killed: ${aliensKilled} x 1000 = +${aliensKilled*1000} points</div>
-                <div class="score" id="shootsFailed">Failed Shoots: ${shootsFailed} x 100 = -${shootsFailed*100} points</div>
+                <div class="score" id="shootsFailed">Failed Shoots: ${shootsFailed} x 500 = -${shootsFailed*500} points</div>
                 <button class= "score" id="btn-restart">RE-START</button>
             <main>
             `
